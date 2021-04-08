a967372c5c8eaa8050c9530c99fcd5d7cda74121
Gökay AKKUŞ <gokayakkus@Gokays-MacBook-Air.local>
2021-04-08 17:03:40 +0300

    ders-8, bootstrap

--- main.py
+++ main.py
@@ -14,6 +14,50 @@ def hakkimizda():
     return render_template("hakkimizda.html")
 
 
+@app.route('/iletisim')
+def iletisim():
+    return render_template("iletisim.html")
+
+
+@app.route('/mesajkaydet', methods=['POST'])
+def mesaj_kaydet():
+    adsoyad = request.form.get('adsoyad')
+    email = request.form.get('email')
+    mesaj = request.form.get('mesaj')
+    satir = adsoyad + "||" + email + "||" + mesaj + "\n"
+    f = open("mesajlar.txt", "a")
+    f.write(satir)
+    f.close()
+    return mesajlar()
+    #return "Sayın " + adsoyad + ". Mesajınız için teşekkürler. Tüm mesajlar için tıklayın."
+
+@app.route('/mesajlar')
+def mesajlar():
+    satirlar = ""
+    mesaj_listesi = []
+    f = open("mesajlar.txt", "r")
+    for satir in f:
+        bilgiler = satir.split("||")
+        adsoyad = bilgiler[0]
+        isimler = adsoyad.split()
+        cnt = 0
+        yildizli_isim = ""
+        for karakter in isimler[0]:
+            if cnt == 0:
+                yildizli_isim += karakter
+                cnt += 1
+            else:
+                yildizli_isim += "*"
+        email=bilgiler[1]
+        mesaj=bilgiler[2]
+        print(bilgiler)
+        satirlar = satirlar + "<b>" + yildizli_isim + "</b> dedi ki: " + mesaj + "<br>"
+        mesaj_listesi.append({"adsoyad":adsoyad,"email":email,"mesaj":mesaj})
+    f.close()
+    return render_template("mesajlar.html", mesaj_listesi=mesaj_listesi)
+
+
+
 @app.route('/hesapla', methods=['POST'])
 def hesapla():
     '''
